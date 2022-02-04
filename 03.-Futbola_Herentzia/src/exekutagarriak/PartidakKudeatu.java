dbd619a6052c65c97737fccda92963c0ffc42136
UNILomana <lomana.markel@uni.eus>
2022-02-04 14:08:02 +0100

    .

--- 03.-Futbola_Herentzia/src/exekutagarriak/PartidakKudeatu.java
+++ 03.-Futbola_Herentzia/src/exekutagarriak/PartidakKudeatu.java
@@ -93,31 +93,36 @@ public class PartidakKudeatu extends Partida {
     }
 
     public static void partidakAsmatu() {
-        /*Partida bakoitzean ArrayList bat dago, beraz sortu egin behar dira hainbat
+        /*Partida bakoitzean ArrayList bat dago, beraz sortu egin behar dira hainbat,
           nahi beste txatelekin*/
         ArrayList<Futbolista> txartelakJarri1 = new ArrayList<Futbolista>();    //1go partidako txartelak
         txartelakJarri1.add((Futbolista) konbokatuak.get(1));                   //1.jokalariari txartela jarri
         txartelakJarri1.add((Futbolista) konbokatuak.get(5));                   //5.jokalariari txartela jarri
         txartelakJarri1.add((Futbolista) konbokatuak.get(6));                   //6.jokalariari txartela jarri
-        
+        txartelakJarri1.add((Futbolista) konbokatuak.get(1));
+
         ArrayList<Futbolista> txartelakJarri2 = new ArrayList<Futbolista>();    //2go partidako txartelak
         txartelakJarri2.add((Futbolista) konbokatuak.get(2));                   //2.jokalariari txartela jarri
         txartelakJarri2.add((Futbolista) konbokatuak.get(3));                   //3.jokalariari txartela jarri
-        
+        txartelakJarri2.add((Futbolista) konbokatuak.get(1));
+        txartelakJarri2.add((Futbolista) konbokatuak.get(1));
+        txartelakJarri2.add((Futbolista) konbokatuak.get(2));
+
         ArrayList<Futbolista> txartelakJarri3 = new ArrayList<Futbolista>();    //3go partidako txartelak
         txartelakJarri3.add((Futbolista) konbokatuak.get(10));                   //10.jokalariari txartela jarri
         txartelakJarri3.add((Futbolista) konbokatuak.get(11));                   //11.jokalariari txartela jarri
-        
-        partidak[++partidaId] = new Partida(LocalDate.parse("2011-02-02"), "Betis", txartelakJarri1);
+        txartelakJarri3.add((Futbolista) konbokatuak.get(1));
+
+        partidak[0] = new Partida(LocalDate.parse("2011-02-02"), "Betis", txartelakJarri1);
         partidak[++partidaId] = new Partida(LocalDate.parse("2020-01-12"), "Barcelona", txartelakJarri2);
-        partidak[++partidaId] = new Partida(LocalDate.parse("2022-06-15"), "fuenla", txartelakJarri3);
+        /* partidak[++partidaId] = new Partida(LocalDate.parse("2022-06-15"), "fuenla", txartelakJarri3);
         partidak[++partidaId] = new Partida(LocalDate.parse("2021-05-31"), "Huesca", txartelakJarri1);      //Txartel berdinak daude jarrita
-        partidak[++partidaId] = new Partida(LocalDate.parse("2019-10-02"), "Tenerife", txartelakJarri3);    ////Txartel berdinak daude jarrita
+        partidak[++partidaId] = new Partida(LocalDate.parse("2019-10-02"), "Tenerife", txartelakJarri3);    ////Txartel berdinak daude jarrita*/
 
     }
 
     public static void partidaBatenDatuakEskatu() {
-        ArrayList<Futbolista> txartelakJarri4 = new ArrayList<Futbolista>();  
+        ArrayList<Futbolista> txartelakJarri4 = new ArrayList<Futbolista>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Noiz jokatu zen partida(Adb: 2020-12-02)? ");
         String data = sc.next();
@@ -125,7 +130,7 @@ public class PartidakKudeatu extends Partida {
         String taldea = sc.next();
         System.out.print("Zenbatgarren jokalariari jarri nahi diozu txartela?");  //Oraingoz bakarrik txartel bat gehitu al da partidan 
         int jokalaria = sc.nextInt();
-        txartelakJarri4.add((Futbolista) konbokatuak.get(jokalaria-1)); 
+        txartelakJarri4.add((Futbolista) konbokatuak.get(jokalaria - 1));
 
         partidak[++partidaId] = new Partida(LocalDate.parse(data), taldea, txartelakJarri4);
     }
@@ -140,14 +145,41 @@ public class PartidakKudeatu extends Partida {
     }
 
     public static void estatistikakKalkulatu() {
-        System.out.println("Jolastutako partida kopurua: " + partidaId);
+        System.out.println("Jolastutako partida kopurua: " + (partidaId + 1));
+
+        //Jokalari bakoitzaren txartelak
+        /*Lortzen dut jokalari baten partida denetako txartelak bistaratzea,
+          orain kontatu egin behar dira*/
+        for (int i = 0; i < konbokatuak.size(); i++) {
+            int count = 0;
+            if (konbokatuak.get(i) != null) {
+                for (int j = 0; j <= partidaId; j++) {                                                      //partida denen artean bilatzeko loop-a, partidaId ipini luzeera erreala ez delako partidak[20]
+                    if (partidak[j] != null) {
+                        for (int k = 0; k < partidak[j].getTxartelakJarri().size(); k++) {                  //partida bakoitzaren barruan loop de txartelak
+                            if (konbokatuak.get(i) == (partidak[j].getTxartelakJarri().get(k))) {
+                                System.out.println(partidak[j].getTxartelakJarri().get(k));
+                                count = count + 1;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+            /*SAIATU. Txartel kopurua bakarrik ikustera txartela duten jokalariek*/
+            System.out.println("Txartel kopurua: " + count);
+        }
     }
+
+    
 
     public static void main(String[] args) {
         selekzioOsoaSortu();
         partidakAsmatu();
-        partidaBatenDatuakEskatu();
-        partidakIkusi();
+        //partidaBatenDatuakEskatu();
+        // partidakIkusi();
         estatistikakKalkulatu();
     }
 
